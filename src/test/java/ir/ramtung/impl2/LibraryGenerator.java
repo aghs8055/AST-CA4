f46e1a2d3fd1a48aa7ecfbf790f1c2ab9ba99b2f
Ali Ghaffari Sabet <aghs8055@gmail.com>
2024-06-11 18:45:36 +0330

    feat: add quickcheck properties for impl2

---
+++ src/test/java/ir/ramtung/impl2/LibraryGenerator.java
@@ -0,0 +1,275 @@
+package ir.ramtung.impl2;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import com.pholser.junit.quickcheck.generator.GenerationStatus;
+import com.pholser.junit.quickcheck.generator.Generator;
+import com.pholser.junit.quickcheck.generator.GeneratorConfiguration;
+import com.pholser.junit.quickcheck.random.SourceOfRandomness;
+import ir.ramtung.sts01.LibraryException;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.util.*;
+
+public class LibraryGenerator extends Generator<Library> {
+    private TestStuMember testStuMember;
+    private TestProfMember testProfMember;
+    private TestBook testBook;
+    private TestMagazine testMagazine;
+    private TestReference testReference;
+    private FixtureCount fixtureCount;
+
+
+    public LibraryGenerator() {
+        super(Library.class);
+    }
+
+    @Override
+    public Library generate(SourceOfRandomness sourceOfRandomness, GenerationStatus generationStatus) {
+        Library library = new Library();
+        List<String> memberNames = new ArrayList<>(Collections.emptyList());
+
+        if (testStuMember != null) {
+            try {
+                library.addStudentMember(testStuMember.stuId(), testStuMember.stuName());
+                memberNames.add("test");
+                memberNames.add(testStuMember.stuName());
+            } catch (LibraryException ignored) {
+            }
+        }
+
+        if (testProfMember != null) {
+            try {
+                library.addProfMember(testProfMember.profName());
+                memberNames.add(testProfMember.profName());
+            } catch (LibraryException ignored) {
+            }
+        }
+
+        if (testBook != null) {
+            try {
+                library.addBook(testBook.title(), testBook.copies());
+            } catch (LibraryException ignored) {
+            }
+        }
+
+        if (testMagazine != null) {
+            try {
+                library.addMagazine(testMagazine.title(), testMagazine.year(), testMagazine.number(), testMagazine.copies());
+            } catch (LibraryException ignored) {
+            }
+        }
+
+        if (testReference != null) {
+            try {
+                library.addReference(testReference.title(), testReference.copies());
+            } catch (LibraryException ignored) {
+            }
+        }
+
+        if (testStuMember != null) {
+            addStudMember(library, fixtureCount.stuCount(), memberNames);
+            addProfMember(library, fixtureCount.profCount(), memberNames);
+            addBook(library, fixtureCount.bookCount());
+            addMagazine(library, fixtureCount.magazineCount());
+            addReference(library, fixtureCount.referenceCount());
+        }
+
+        try {
+            simulateTimePass(library, fixtureCount.days(), memberNames);
+        } catch (LibraryException ignored) {
+        }
+
+        return library;
+    }
+
+    public void simulateTimePass(Library library, int time, List<String> memberNames) throws LibraryException {
+        List<List<String>> borrowedBooks = new ArrayList<>(Collections.emptyList());
+        Random random = new Random();
+
+        if (memberNames.isEmpty()) {
+            library.timePass(time);
+            return;
+        }
+
+        for (int i = 0; i < time; i++) {
+            int borrowCount = randomInt();
+            for (int j = 0; j < borrowCount; j++) {
+                List<String> availableTitles = library.availableTitles();
+
+                if (!availableTitles.isEmpty()) {
+                    try {
+                        String member = memberNames.get(random.nextInt(memberNames.size()));
+                        String title = availableTitles.get(random.nextInt(availableTitles.size()));
+                        library.borrow(member, title);
+                        borrowedBooks.add(Arrays.asList(member, title));
+                    } catch (LibraryException ignored) {
+                    }
+                }
+            }
+
+            if (!borrowedBooks.isEmpty()) {
+                int extendCount = random.nextInt(borrowedBooks.size());
+                for (int j = 0; j < extendCount; j++) {
+                    try {
+                        List<String> borrowedBook = borrowedBooks.get(random.nextInt(borrowedBooks.size()));
+                        library.extend(borrowedBook.get(0), borrowedBook.get(1));
+                    } catch (LibraryException ignored) {
+                    }
+                }
+
+
+                int returnCount = random.nextInt(borrowedBooks.size());
+                for (int j = 0; j < returnCount; j++) {
+                    try {
+                        List<String> borrowedBook = borrowedBooks.get(random.nextInt(borrowedBooks.size()));
+                        library.returnDocument(borrowedBook.get(0), borrowedBook.get(1));
+                        borrowedBooks.remove(borrowedBook);
+                    } catch (Exception ignored) {
+                    }
+                }
+            }
+
+            library.timePass(1);
+        }
+    }
+
+    public void addStudMember(Library library, int count, List<String> memberNames) {
+        for (int i = 0; i < count; i++) {
+            try {
+                String stuName = randomString();
+                library.addStudentMember(randomString(), stuName);
+                memberNames.add(stuName);
+            } catch (LibraryException ignored) {
+            }
+        }
+    }
+
+    public void addProfMember(Library library, int count, List<String> memberNames) {
+        for (int i = 0; i < count; i++) {
+            try {
+                String profName = randomString();
+                library.addProfMember(profName);
+                memberNames.add(profName);
+            } catch (LibraryException ignored) {
+            }
+        }
+    }
+
+    public void addBook(Library library, int count) {
+        for (int i = 0; i < count; i++) {
+            try {
+                library.addBook(randomString(), randomInt());
+            } catch (LibraryException ignored) {
+            }
+        }
+    }
+
+    public void addMagazine(Library library, int count) {
+        for (int i = 0; i < count; i++) {
+            try {
+                library.addMagazine(randomString(), randomInt(), randomInt(), randomInt());
+            } catch (LibraryException ignored) {
+            }
+        }
+    }
+
+    public void addReference(Library library, int count) {
+        for (int i = 0; i < count; i++) {
+            try {
+                library.addReference(randomString(), randomInt());
+            } catch (LibraryException ignored) {
+            }
+        }
+    }
+
+    private static String randomString() {
+        return UUID.randomUUID().toString();
+    }
+
+    private static int randomInt() {
+        return (int) (Math.random() * 100 + 1);
+    }
+
+    public void configure(TestStuMember testStuMember) {
+        this.testStuMember = testStuMember;
+    }
+
+    public void configure(TestProfMember testProfMember) {
+        this.testProfMember = testProfMember;
+    }
+
+    public void configure(TestBook testBook) {
+        this.testBook = testBook;
+    }
+
+    public void configure(TestMagazine testMagazine) {
+        this.testMagazine = testMagazine;
+    }
+
+    public void configure(TestReference testReference) {
+        this.testReference = testReference;
+    }
+
+    public void configure(FixtureCount fixtureCount) {
+        this.fixtureCount = fixtureCount;
+    }
+
+    @Target({ PARAMETER, FIELD, ANNOTATION_TYPE, TYPE_USE })
+    @Retention(RUNTIME)
+    @GeneratorConfiguration
+    public @interface TestStuMember {
+        String stuName();
+        String stuId();
+    }
+
+        @Target({ PARAMETER, FIELD, ANNOTATION_TYPE, TYPE_USE })
+        @Retention(RUNTIME)
+        @GeneratorConfiguration
+        public @interface TestProfMember {
+            String profName();
+        }
+
+    @Target({ PARAMETER, FIELD, ANNOTATION_TYPE, TYPE_USE })
+    @Retention(RUNTIME)
+    @GeneratorConfiguration
+    public @interface TestBook {
+        String title();
+        int copies();
+    }
+
+    @Target({ PARAMETER, FIELD, ANNOTATION_TYPE, TYPE_USE })
+    @Retention(RUNTIME)
+    @GeneratorConfiguration
+    public @interface TestMagazine {
+        String title();
+        int number();
+        int year();
+        int copies();
+    }
+
+    @Target({ PARAMETER, FIELD, ANNOTATION_TYPE, TYPE_USE })
+    @Retention(RUNTIME)
+    @GeneratorConfiguration
+    public @interface TestReference {
+        String title();
+        int copies();
+    }
+
+    @Target({ PARAMETER, FIELD, ANNOTATION_TYPE, TYPE_USE })
+    @Retention(RUNTIME)
+    @GeneratorConfiguration
+    public @interface FixtureCount {
+        int stuCount() default 10;
+        int profCount() default 10;
+        int bookCount() default 10;
+        int magazineCount() default 10;
+        int referenceCount() default 10;
+        int days() default 10;
+    }
+}
---
+++ src/test/java/ir/ramtung/impl2/LibraryPropertiesImpl2Test.java
@@ -0,0 +1,274 @@
+package ir.ramtung.impl2;
+
+import com.pholser.junit.quickcheck.From;
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+import ir.ramtung.sts01.LibraryException;
+import org.junit.runner.RunWith;
+import ir.ramtung.impl2.LibraryGenerator.TestStuMember;
+import ir.ramtung.impl2.LibraryGenerator.TestProfMember;
+import ir.ramtung.impl2.LibraryGenerator.TestBook;
+import ir.ramtung.impl2.LibraryGenerator.TestReference;
+import ir.ramtung.impl2.LibraryGenerator.TestMagazine;
+import ir.ramtung.impl2.LibraryGenerator.FixtureCount;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+@RunWith(JUnitQuickcheck.class)
+public class LibraryPropertiesImpl2Test {
+
+    @Property
+    public void nameCheckingInAddStudentMember(String studentId, String studentName) {
+        Library library = new Library();
+
+        try {
+            library.addStudentMember(studentId, studentName);
+
+            if (studentId.isEmpty() || studentName.isEmpty())
+                fail("Expected exception while adding student member with empty id or name");
+        } catch (LibraryException e) {
+            if (!studentId.isEmpty() && !studentName.isEmpty())
+                fail("Got exception while adding student member with non empty name and id: " + e.getMessage());
+        } catch (Exception ignored) {
+            fail("Got an unexpected error");
+        }
+    }
+
+    @Property
+    public void equalityOfAddedPenaltyForEachDayAfterTenDays(
+            @From(LibraryGenerator.class)
+            @FixtureCount()
+            @TestStuMember(stuName = "Ali", stuId = "123")
+            Library library
+    ) {
+        try {
+            library.timePass(55);
+            int penalty1 = library.getTotalPenalty("Ali");
+            library.timePass(1);
+            int penalty2 = library.getTotalPenalty("Ali");
+            library.timePass(1);
+            int penalty3 = library.getTotalPenalty("Ali");
+            assertEquals(penalty2 - penalty1, penalty3 - penalty2);
+        } catch (LibraryException e) {
+            fail("Got exception while getting total penalty for existing member: " + e.getMessage());
+        } catch (Exception ignored) {
+            fail("Got an unexpected error");
+        }
+
+    }
+
+    @Property
+    public void illegalityOfExtendAtDayOfBorrowing(
+            @From(LibraryGenerator.class)
+            @FixtureCount()
+            @TestStuMember(stuName = "Ali", stuId = "123")
+            @TestBook(title = "Book", copies = 1)
+            Library library
+    ) {
+        try {
+            library.borrow("Ali", "Book");
+        } catch (LibraryException ignored) {
+            return;
+        }
+
+        try {
+            library.extend("Ali", "Book");
+            fail("Expected exception while extending at day of borrowing");
+        } catch (LibraryException ignored) {
+        } catch (Exception ignored) {
+            fail("Got an unexpected error");
+        }
+    }
+
+    @Property
+    public void legalityOfTwoExtendsBeforeReturningDate(
+            @From(LibraryGenerator.class)
+            @FixtureCount(stuCount = 0, profCount = 0, bookCount = 0, magazineCount = 0, referenceCount = 0)
+            @TestStuMember(stuName = "Ali", stuId = "123")
+            @TestBook(title = "Book", copies = 1)
+            Library library
+    ) {
+        try {
+            library.returnDocument("Ali", "Book");
+        } catch (Exception ignored) {
+            fail("Got an unexpected error");
+        }
+
+        try {
+            library.borrow("Ali", "Book");
+        } catch (LibraryException e) {
+            fail("Got exception while borrowing book: " + e.getMessage());
+        } catch (Exception ignored) {
+            fail("Got an unexpected error");
+        }
+
+        try {
+            library.timePass(1);
+            library.extend("Ali", "Book");
+            library.timePass(1);
+            library.extend("Ali", "Book");
+        } catch (LibraryException e) {
+            fail("Got exception while extending book: " + e.getMessage());
+        } catch (Exception ignored) {
+            fail("Got an unexpected error");
+        }
+    }
+
+    @Property
+    public void illegalityOfMoreThanTwoExtendsBeforeReturningDate(
+            @From(LibraryGenerator.class)
+            @FixtureCount(stuCount = 0, profCount = 0, bookCount = 0, magazineCount = 0, referenceCount = 0)
+            @TestStuMember(stuName = "Ali", stuId = "123")
+            @TestBook(title = "Book", copies = 1)
+            Library library
+    ) {
+        try {
+            library.returnDocument("Ali", "Book");
+        } catch (Exception ignored) {
+            fail("Got an unexpected error");
+        }
+
+        try {
+            library.borrow("Ali", "Book");
+        } catch (LibraryException e) {
+            fail("Got exception while borrowing book: " + e.getMessage());
+        } catch (Exception ignored) {
+            fail("Got an unexpected error");
+        }
+
+        try {
+            library.timePass(1);
+            library.extend("Ali", "Book");
+            library.timePass(1);
+            library.extend("Ali", "Book");
+            library.timePass(1);
+            library.extend("Ali", "Book");
+            fail("Expected exception while extending more than two times before returning date");
+        } catch (LibraryException ignored) {
+        } catch (Exception ignored) {
+            fail("Got an unexpected error");
+        }
+    }
+
+    @Property
+    public void availabilityOfFullyBorrowedBookAfterReturningOneCopy(
+            @From(LibraryGenerator.class)
+            @FixtureCount(stuCount = 0, profCount = 0, bookCount = 0, magazineCount = 0, referenceCount = 0)
+            @TestStuMember(stuName = "Ali", stuId = "123")
+            @TestBook(title = "Book", copies = 2)
+            Library library
+    ) {
+        try {
+            library.borrow("Ali", "Book");
+            library.borrow("Ali", "Book");
+        } catch (LibraryException ignored) {
+        } catch (Exception ignored) {
+            fail("Got an unexpected error");
+        }
+
+        assertEquals(0, library.availableTitles().size());
+
+        try {
+            library.returnDocument("Ali", "Book");
+        } catch (Exception ignored) {
+            fail("Got an unexpected error");
+        }
+
+        assertEquals(1, library.availableTitles().size());
+    }
+
+    @Property
+    public void illegalityOfBorrowingMoreThanFiveDocumentsByProf(
+            @From(LibraryGenerator.class)
+            @FixtureCount(stuCount = 0, profCount = 0, bookCount = 0, magazineCount = 0, referenceCount = 0)
+            @TestProfMember(profName = "Prof. Ali")
+            @TestBook(title = "Book", copies = 10)
+            Library library
+    ) {
+        for (int i = 0; i < 5; i++) {
+            try {
+                library.returnDocument("Prof. Ali", "Book");
+            } catch (Exception ignored) {
+                fail("Got an unexpected error");
+            }
+        }
+
+        try {
+            library.borrow("Prof. Ali", "Book");
+            library.borrow("Prof. Ali", "Book");
+            library.borrow("Prof. Ali", "Book");
+            library.borrow("Prof. Ali", "Book");
+            library.borrow("Prof. Ali", "Book");
+        } catch (LibraryException e) {
+            fail("Got an exception while borrowing less that six documents by prof: " + e.getMessage());
+        } catch (Exception ignored) {
+            fail("Got an unexpected error");
+        }
+
+        try {
+            library.borrow("Prof. Ali", "Book");
+            fail("Expected exception while borrowing more than five documents by prof");
+        } catch (LibraryException ignored) {
+        } catch (Exception ignored) {
+            fail("Got an unexpected error");
+        }
+    }
+
+    @Property
+    public void correctnessOfComputingPenaltyForDocument(
+            @From(LibraryGenerator.class)
+            @FixtureCount(stuCount = 0, profCount = 0, bookCount = 0, magazineCount = 0, referenceCount = 0)
+            @TestStuMember(stuName = "Ali", stuId = "123")
+            @TestReference(title = "Reference", copies = 2)
+            Library library
+    ) {
+        try {
+            library.returnDocument("Ali", "Reference");
+        } catch (Exception ignored) {
+            fail("Got an unexpected error");
+        }
+
+        try {
+            library.timePass(60);
+            int penalty1 = library.getTotalPenalty("Ali");
+            library.timePass(1);
+            int penalty2 = library.getTotalPenalty("Ali");
+            int penaltyDiff = penalty2 - penalty1;
+            library.borrow("Ali", "Reference");
+            library.timePass(5);
+            assertEquals(library.getTotalPenalty("Ali"), penalty2 + 5 * penaltyDiff);
+            library.timePass(1);
+            assertEquals(library.getTotalPenalty("Ali"), penalty2 + 6 * penaltyDiff + 5000);
+            library.timePass(3);
+            assertEquals(library.getTotalPenalty("Ali"), penalty2 + 9 * penaltyDiff + 22000);
+        } catch (LibraryException e) {
+            fail("Got exception while passing time: " + e.getMessage());
+        } catch (Exception ignored) {
+            fail("Got an unexpected error");
+        }
+    }
+    
+    @Property
+    public void illegalityOfExtendingMagazine(
+            @From(LibraryGenerator.class)
+            @FixtureCount()
+            @TestStuMember(stuName = "Ali", stuId = "123")
+            @TestMagazine(title = "Magazine", copies = 1, year = 1400, number = 1)
+            Library library
+    ) {
+        try {
+            library.borrow("Ali", "Magazine");
+        } catch (LibraryException ignored) {
+            return;
+        }
+
+        try {
+            library.extend("Ali", "Magazine");
+            fail("Expected exception while extending magazine");
+        } catch (LibraryException ignored) {
+        } catch (Exception ignored) {
+            fail("Got an unexpected error");
+        }
+    }
+}
